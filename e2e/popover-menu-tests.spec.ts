5d69f523f0f51a01bd32c73eef55a184e513dd0e
Joshua Neuenhoff <5409190+JoshuaEN@users.noreply.github.com>
2023-05-26 19:21:11 -0400

    Add tests around clicking a tile with another tile's popover menu already opened

--- e2e/popover-menu-tests.spec.ts
+++ e2e/popover-menu-tests.spec.ts
@@ -1,7 +1,8 @@
 import { expect, test } from "@playwright/test";
+import { TileState } from "~/src/game/types/tile-states.js";
 import { GameBoardHarness } from "~/test/playwright/game-board.harness.js";
 
-test("it should open the menu when clicking on a tile", async ({
+test("it should open the menu when clicking on a blank tile", async ({
   page,
   request,
   context,
@@ -40,4 +41,50 @@ test("it should close the menu when clicking on a tile when the tile's menu is o
 
   // Assert
   await expect(harness.getPopover()).not.toBeVisible();
+});
+test("it should re-open the menu when clicking on a different tile than the tile of the currently opened menu", async ({
+  page,
+  request,
+  context,
+}) => {
+  // Arrange
+  await page.goto(".");
+  const harness = new GameBoardHarness(page.locator("html"), {
+    page,
+    request,
+    context,
+  });
+
+  // Act
+  await harness.getTileLocator(0).click();
+  await expect(harness.getPopover()).toBeVisible();
+  await harness.getTileLocator(1).click();
+
+  // Assert
+  await expect(harness.getPopover()).toBeVisible();
+});
+
+test("it should re-open the menu when clicking on a different tile than the tile of the currently opened menu when in FillSword (regression test)", async ({
+  page,
+  request,
+  context,
+}) => {
+  // Arrange
+  await page.goto(".");
+  const harness = new GameBoardHarness(page.locator("html"), {
+    page,
+    request,
+    context,
+  });
+  await harness.setUserSelection(7, TileState.Blocked);
+  await harness.setUserSelection(25, TileState.Blocked);
+  await harness.setUserSelection(22, TileState.Sword);
+
+  // Act
+  await harness.getTileLocator(22).click();
+  await expect(harness.getPopover()).toBeVisible();
+  await harness.getTileLocator(21).click();
+
+  // Assert
+  await expect(harness.getPopover()).toBeVisible();
 });
