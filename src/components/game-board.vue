475d8230c3639640090dedf4c4f4daf441b74d5d
Joshua Neuenhoff <5409190+JoshuaEN@users.noreply.github.com>
2025-01-16 19:47:35 -0500

    Fix popover preferring shrinking over shifting when on the right edge Prevent arrow being shifted off alignment with the popover.

--- src/components/game-board.vue
+++ src/components/game-board.vue
@@ -14,6 +14,7 @@ import {
   shift,
   autoUpdate,
   arrow,
+  limitShift,
 } from "@floating-ui/vue";
 import BaseTile from "~/src/components/base-tile.vue";
 import { getPickerOptions } from "./game-board.utils.js";
@@ -31,7 +32,7 @@ const popoverArrowRef = ref(null);
 const popoverMiddleware = ref([
   offset({ mainAxis: 10 }),
   flip(),
-  shift({ padding: 16 }),
+  shift({ padding: 16, limiter: limitShift({ offset: 15 /* Arrow width */ }) }),
   arrow({
     element: computed(() => {
       const val = popoverArrowRef.value;
