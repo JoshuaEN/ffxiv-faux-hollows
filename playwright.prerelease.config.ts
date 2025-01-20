c8606647039b89e651b71fb0e80f8f5e17b05d10
Joshua Neuenhoff <5409190+JoshuaEN@users.noreply.github.com>
2025-01-20 17:33:48 -0500

    Shorten timeout and upload webserver logs

--- playwright.prerelease.config.ts
+++ playwright.prerelease.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     ignoreHTTPSErrors: true,
   },
 
-  timeout: 10 * 60 * 1000,
+  timeout: 3 * 60 * 1000,
 
   /* Configure projects for major browsers */
   projects: [
