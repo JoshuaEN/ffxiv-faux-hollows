d46a7a4c125ba7dc2c3c7b65fec479e782aa5752
Joshua Neuenhoff <5409190+JoshuaEN@users.noreply.github.com>
2025-01-20 01:00:37 -0500

    Lines too

--- playwright.prerelease.config.ts
+++ playwright.prerelease.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: isCI ? 1 : Math.floor(cpus().length * 0.75),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "blob",
+  reporter: [["line"], ["blob"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
