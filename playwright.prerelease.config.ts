29c959397541499274e43900f43fa3879e9a12d8
Joshua Neuenhoff <5409190+JoshuaEN@users.noreply.github.com>
2025-01-20 01:26:24 -0500

    Fix some browsers not working with prod headers

--- playwright.prerelease.config.ts
+++ playwright.prerelease.config.ts
@@ -27,10 +27,12 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:8788",
+    baseURL: "https://localhost:8788",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+
+    ignoreHTTPSErrors: true,
   },
 
   timeout: 10 * 60 * 1000,
@@ -76,7 +78,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "pnpm run preview",
-    url: "http://localhost:8788",
+    url: "https://localhost:8788",
     reuseExistingServer: false,
+    ignoreHTTPSErrors: true,
   },
 });
